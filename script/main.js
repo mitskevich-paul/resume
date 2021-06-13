a619d6def1fee51919e683c2f4a1d53635914fb1
mitskevich-paul <58680035+mitskevich-paul@users.noreply.github.com>
2021-06-13 18:38:09 +0300

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,5 +1,6 @@
 let head = document.querySelector('header');
 let menu = document.querySelector('.menu');
+let openPhotoIndex = 0;
 document.addEventListener('scroll', () => {
     if (window.pageYOffset >= head.clientHeight) {
         menu.style.cssText = `
@@ -15,22 +16,52 @@ document.addEventListener('scroll', () => {
         `
     }
 }) 
-let portfolioArray = document.querySelectorAll('.portfolio-project');
-portfolioArray[0].addEventListener('click', () => {
-    window.open('http://fixteam.by');
-})
-portfolioArray[1].addEventListener('click', () => {
-    window.open('http://stivmebel.by');
-})
-portfolioArray[2].addEventListener('click', () => {
-    window.open('https://skladok.by');
-})
-portfolioArray[3].addEventListener('click', () => {
-    window.open('https://Olmeko-mebel.by);
-})
-portfolioArray[4].addEventListener('click', () => {
-    window.open('https://mitskevich-paul.github.io/fixteam/');
-})
-portfolioArray[5].addEventListener('click', () => {
-    window.open('https://mitskevich-paul.github.io/');
+let linksContainer = document.querySelectorAll('.portfolio-project-link');
+document.addEventListener('click', event => {
+    if (event.target === linksContainer[0]) {
+        window.open('http://fixteam.by');
+    } else if (event.target === linksContainer[1]) {
+        window.open('http://stivmebel.by');
+    } else if (event.target === linksContainer[2]) {
+        window.open('https://skladok.by');
+    } else if (event.target === linksContainer[3]) {
+        window.open('https://mitskevich-paul.github.io/olmeko/');
+    } else if (event.target === linksContainer[4]) {
+        window.open('https://mitskevich-paul.github.io/fixteam/');
+    } else if (event.target === linksContainer[5]) {
+        window.open('https://mitskevich-paul.github.io/');
+    } 
+
+    if (event.target.classList.contains('about-me_right-side') && openPhotoIndex === 0) {
+        event.target.style.cssText = `
+            z-index: 666;
+            position: fixed;
+            width: 100%;
+            height: 100vh;
+            top: 0;
+            left: 0;
+            background: center / cover no-repeat url("./sources/me.jpg");
+            background-color: #fff;
+        `
+        openPhotoIndex = 1;
+    } else if (event.target.classList.contains('about-me_right-side') && openPhotoIndex === 1) {
+        event.target.style.cssText = '';
+        openPhotoIndex = 0;
+    }
+    if (event.target.classList.contains('level-of-english') && openPhotoIndex === 0) {
+        event.target.style.cssText = `
+            margin: 0;
+            z-index: 666;
+            position: fixed;
+            height: 100vh;
+            top: 0;
+            left: 50%;
+            transform: translateX(-50%);
+            background-color: #fff;
+        `
+        openPhotoIndex = 1;
+    } else if (event.target.classList.contains('level-of-english') && openPhotoIndex === 1) {
+        event.target.style.cssText = '';
+        openPhotoIndex = 0;
+    }
 })
